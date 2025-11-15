db9d8b801f9654dc9dd76af9877dacb4980806f9
Ondrej Wisniewski <ondrej.wisniewski@dekitalia.com>
2025-11-15 19:13:08 +0100

    Switch to asyncio architecture

---
+++ src/globals.py
@@ -0,0 +1,12 @@
+class Globals():
+   # Object handles
+    car = None
+    led = None
+    neo = None
+    srv = None
+    bat = None
+    buz = None
+    ult = None
+    trk = None
+    lit = None
+    
--- src/main.py
+++ src/main.py
@@ -1,80 +1,44 @@
  
 import freenove_4wd_hardware
+import net
 from time import sleep
+import asyncio
+from globals import Globals
 
 DELAY_ACTION = 2
 DELAY_WAIT   = 2
+       
+
+async def sensormon():
+    print("Start sensor monitor")
+    while True:
+        await asyncio.sleep(2)
+        try:
+            print("battery voltage is %.2fV (%d%%)" % (Globals.bat.voltage(),Globals.bat.perc()))
+            print("obstacle %.2f cm ahead" % Globals.ult.distance())
+            print("track status is %d-%d-%d" % Globals.trk.status())
+            print("light level left %.1f%% / right %.1f%%" % Globals.lit.level())
+        except TypeError:
+            pass
+        print()
 
 
-#######################
-# CAR class
-#######################
-class CAR:
-    def __init__(self,m1,m2,m3,m4):
-        self._m1 = m1
-        self._m2 = m2
-        self._m3 = m3
-        self._m4 = m4
-        
-    def forward(self,speed):
-        speed = abs(speed)
-        # Front wheels
-        self._m1.move(speed)
-        self._m4.move(speed)
-        # Back wheels
-        self._m2.move(speed)
-        self._m3.move(speed)
+###################################################
+# MAIN FUNCTION ON CORE 0
+###################################################
+async def main_core0():
+    # Start all modules as concurrent tasks
+    # Each module executes an infinite loop
+    await asyncio.gather(net.webserver(),
+                         sensormon()
+                        )
 
-    def backward(self,speed):
-        speed = -abs(speed)
-        # Front wheels
-        self._m1.move(speed)
-        self._m4.move(speed)
-        # Back wheels
-        self._m2.move(speed)
-        self._m3.move(speed)
-    
-    def spin_right(self):
-        speed = 30
-        # Left wheels
-        self._m1.move(speed)
-        self._m2.move(speed)
-        # Right wheels
-        self._m3.move(-speed)
-        self._m4.move(-speed)
-        
-    def spin_left(self):
-        speed = 30
-        # Left wheels
-        self._m1.move(-speed)
-        self._m2.move(-speed)
-        # Right wheels
-        self._m3.move(speed)
-        self._m4.move(speed)
 
-    def turn_right(self,speed):
-        # Left wheels
-        self._m1.move(speed)
-        self._m2.move(speed)
-        # Right wheels
-        self._m3.move(0)
-        self._m4.move(0)
-        
-    def turn_left(self,speed):
-        # Left wheels
-        self._m1.move(0)
-        self._m2.move(0)
-        # Right wheels
-        self._m3.move(speed)
-        self._m4.move(speed)
-        
-    def stop(self):
-        self._m1.stop()
-        self._m2.stop()
-        self._m3.stop()
-        self._m4.stop()
-        
-        
+def run_core0():
+    print("Start CORE 0 loop")
+    asyncio.run(main_core0())
+
+
 ###########################################################
 #
 #        MAIN PROGRAM
@@ -84,44 +48,48 @@ class CAR:
 # Init motors
 (m1,m2,m3,m4) = freenove_4wd_hardware.init_motors()
 
+# Init car
+Globals.car = freenove_4wd_hardware.init_car(m1,m2,m3,m4)
+
 # Init led
-led = freenove_4wd_hardware.init_led()
-led.on()
+Globals.led = freenove_4wd_hardware.init_led()
+Globals.led.on()
 
 # Init Neopixel LED strip
-neo = freenove_4wd_hardware.init_ledstrip()
-neo.walk("R")
-neo.walk("G")
-neo.walk("B")
-sleep(DELAY_WAIT)
+Globals.neo = freenove_4wd_hardware.init_ledstrip()
+#neo.walk("R")
+#neo.walk("G")
+#neo.walk("B")
+#sleep(DELAY_WAIT)
 
 # Init servo
-srv = freenove_4wd_hardware.init_servo()
-srv.sweep()
+Globals.srv = freenove_4wd_hardware.init_servo()
+#srv.sweep()
+#sleep(DELAY_WAIT)
 
 # Init battery monitor
-bat = freenove_4wd_hardware.init_batt_mon()
-sleep(DELAY_WAIT)
+Globals.bat = freenove_4wd_hardware.init_batt_mon()
+#sleep(DELAY_WAIT)
 
 # Init buzzer
-buz = freenove_4wd_hardware.init_buzzer()
-buz.beep()
+Globals.buz = freenove_4wd_hardware.init_buzzer()
+#buz.beep()
 
 # Init Ultrasonic sensor
-ult = freenove_4wd_hardware.init_ultrasonic(buzzer=buz)
-sleep(DELAY_WAIT)
+Globals.ult = freenove_4wd_hardware.init_ultrasonic(buzzer=Globals.buz)
+#sleep(DELAY_WAIT)
 
 # Init Track sensor
-trk = freenove_4wd_hardware.init_track(buzzer=buz)
-sleep(DELAY_WAIT)
+Globals.trk = freenove_4wd_hardware.init_track(buzzer=Globals.buz)
+#sleep(DELAY_WAIT)
 
 # Init Light sensor
-lit = freenove_4wd_hardware.init_light()
-sleep(DELAY_WAIT)
+Globals.lit = freenove_4wd_hardware.init_light()
+#sleep(DELAY_WAIT)
 
-# Init car
-car = CAR(m1,m2,m3,m4)
-sleep(DELAY_WAIT)
+# Init network
+net.connect_network()
+
 
 '''
 # Move forward
@@ -163,11 +131,5 @@ sleep(DELAY_WAIT)
 n.blink("G")
 '''
 
-while True:
-    sleep(2)
-    print("battery voltage is %.2fV (%d%%)" % (bat.voltage(),bat.perc()))
-    print("obstacle %.2f cm ahead" % ult.distance())
-    print("track status is %d-%d-%d" % trk.status())
-    print("light level left %.1f%% / right %.1f%%" % lit.level())
-    print()
-    
+# Start services
+run_core0()
